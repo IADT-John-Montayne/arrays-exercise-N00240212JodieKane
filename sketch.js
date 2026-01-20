dd1c5cafe32b45b33e9fc3d3be3a3d5231805a48
N00240212JodieKane <N00240212@iadt.ie>
2026-01-20 13:33:22 +0000

    Finished Excercise For Day 1

--- sketch.js
+++ sketch.js
@@ -41,7 +41,50 @@ console.log(fruit);
 
 
 
+//Excercise Fruit Basket Manager Tasks 
+let basket = ['apple', 'banana', 'orange', 'pear'];
 
+//Read from the array
+
+//log the first fruit
+console.log(basket[0]);
+
+//log the last fruit use length
+console.log(basket.length); 
+console.log(basket.indexOf('pear'));
+console.log('The last fruit in the console/basket is '  +basket[3]);
+
+//Modify the basket
+
+//replace banana with kiwi
+basket[1] = 'kiwi';
+console.log(basket)
+
+//remove the last fruit
+basket.pop();
+console.log(basket);
+
+//add mango to the start of the array
+basket.unshift('mango');
+console.log(basket);
+
+//loop through the basket
+for (let i = 0; i < basket.length; i++) {
+	console.log('Fruit ' + i + ': ' + basket[i]) // + i + gives the number of which number belongs to which fruit
+}
+
+//Search for a fruit
+console.log(basket.indexOf('orange')); //Orange is 3
+
+//If it exists or does not exist
+if (basket.indexOf('orange' === 3)) { //Since its 3 it will be orange if not it will not exist 
+	console.log('There is an orange in the basket');} else {
+		console.log ('There is no existing orange unfortunately')
+	} //The if else statement
+
+//Copy part of any basket
+let citrus1 = basket.splice(0, 3);
+console.log(citrus1);
 
 function setup() {
 	createCanvas(500, 500);
